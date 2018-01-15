2c58a4703dd374648b2f1239e8a2847044850e60
Anthony <anthony.caccavella@gmail.com>
2018-01-15 13:52:24 -0700

    rebuilt and deployed - result is blank deployed spa

--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ app.listen(port, () => {
 
 const path = require('path')
 app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../../build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 })
